9fe77deecc0fbd6f85c3668631a462a64be269d2
dbetke <danielle.betke@gmail.com>
2012-10-08 15:14:16 -0400

    adds jquery library, creates a jquery message box

--- script/script.js
+++ script/script.js
@@ -4,10 +4,13 @@
 var containerDiv = "#imageContainer";
 
 function init(){
+
 	$(containerDiv).click(addErrors); 
 
 	//adds the necessary css for the errorDisplay
-	$("head").append("<style type=\"text/css\">#errorDisplay{background-color: gray;width: 100%; height: 20px;position: relative;top: 95%;margin: 0 auto;text-align: left;</style>}");
+	//$("head").append("<style type=\"text/css\">#errorDisplay{background-color: gray;width: 100%; height: 20px;position: relative;top: 95%;margin: 0 auto;text-align: left;</style>}");
+
+
 
 }
 
@@ -16,8 +19,25 @@ function addErrors(){
 	$('#imageContainer').append('<div id ="errorDisplay"></div>');
 	//var msgString = $("This is an error <br />"); 
 	
-	$("#errorDisplay").append("This is an error!<br />");
+	//$("#errorDisplay").append("This is an error!<br />");
 	//$("#errorDisplay").append(msgString); 
 
 
+/**
+	Download necessary files for locally stored css and js for dialog
+	Once verified, change to have dialog hover in the proper area
+	Change css to have dialog box be the right size relative to the
+		container
+	Have dialog not force user to click - simply sit there and allow the 
+		user to ignore or x out
+*/
+
+
+	$('#errorDisplay').append('<div id ="dialog"></div>');
+	$( "#dialog" ).dialog({ height: 75, minWidth:500, position: "bottom" });
+	//$(".ui-dialog-titlebar").hide();
+	//$(".ui-dialog").addClass("customclass");
+
 }
+
+
