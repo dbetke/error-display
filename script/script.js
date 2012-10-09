5a1a363001eb8effc56a83c9a16add4518fb41e8
dbetke <danielle.betke@gmail.com>
2012-10-09 14:42:49 -0400

    changes message box to display relative to the assigned container div, adjusts height, width, and position

--- script/script.js
+++ script/script.js
@@ -1,42 +1,23 @@
 
 
-//will need to change div ID depending page adding error display to
-var containerDiv = "#imageContainer";
+var containerDiv = "#imageContainer";  //div id to place the error dialog in, designed to change as needed
 
 function init(){
 
-	$(containerDiv).click(addErrors); 
-
-	//adds the necessary css for the errorDisplay
-	//$("head").append("<style type=\"text/css\">#errorDisplay{background-color: gray;width: 100%; height: 20px;position: relative;top: 95%;margin: 0 auto;text-align: left;</style>}");
-
-
+	addErrors();  //adds the error display message box on page load
 
 }
 
 function addErrors(){
-	//appends the error div to the container
-	$('#imageContainer').append('<div id ="errorDisplay"></div>');
-	//var msgString = $("This is an error <br />"); 
-	
-	//$("#errorDisplay").append("This is an error!<br />");
-	//$("#errorDisplay").append(msgString); 
+	$(containerDiv).append('<div id ="dialog"></div>');  //appends the dialog div to the container
+
+    var w = $(containerDiv).width() - 15;  //calculates the necessary width relative to the container div
+    var h = $(containerDiv).height() * .25;  //calculates the necessary height relative to the container div
 
 
-/**
-	Download necessary files for locally stored css and js for dialog
-	Once verified, change to have dialog hover in the proper area
-	Change css to have dialog box be the right size relative to the
-		container
-	Have dialog not force user to click - simply sit there and allow the 
-		user to ignore or x out
-*/
 
-
-	$('#errorDisplay').append('<div id ="dialog"></div>');
-	$( "#dialog" ).dialog({ height: 75, minWidth:500, position: "bottom" });
-	//$(".ui-dialog-titlebar").hide();
-	//$(".ui-dialog").addClass("customclass");
+	$( "#dialog" ).dialog({ height: h, width: w, position: { my: "center bottom", at: "center bottom", of: containerDiv} }); //adds the dialog box
+	$('div.ui-dialog-titlebar').css('height', '.5em'); //reduces the height of the title bar
 
 }
 
