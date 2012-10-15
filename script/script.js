e8f1dcc36e54df233366b908b1d3e8046a66929b
dbetke <danielle.betke@gmail.com>
2012-10-15 14:57:18 -0400

    adds ability for jquery to append the error display based on the parent div, where the user adds the div in their html page

--- script/script.js
+++ script/script.js
@@ -1,9 +1,4 @@
 
-
-var containerDiv = "#imageContainer";  //div id to place the error dialog in, designed to change as needed
-	
-
-
 function init(){
 	var msg = "This is a string of errors \n with a second line of errors \n and a third line of errors";  //assigning a message string for testing
 	
@@ -11,23 +6,28 @@ function init(){
 
 }
 
+
+
 function addErrors(msgString){
-	
-	$(containerDiv).append('<div id ="dialog" title="Errors"></div>'); //appends the errors div to the container declared above
 
+	var containerDiv = "#" + $("#errorDisplay").parent().attr("id"); //finds the parent div to append the message dialog to
 
-	$(containerDiv).append('<div id ="dialog" title="Errors"></div>'); //appends the errors div to the container declared above
+	if (msgString != null){ //only create error message dialog if errors exist
 
-    var w = $(containerDiv).width() - 15;  //calculates the necessary width relative to the container div
-    var h = $(containerDiv).height() * .30;  //calculates the necessary height relative to the container div
+		$(containerDiv).append('<div id ="dialog" title="Errors"></div>'); //appends the errors div to the container declared above
 
-    $("#dialog").html(msgString.replace(/\n\r?/g, '<br />')); //replace all \n's with <br />'s, since \n is not recognized in the dialog box
-    $("#dialog").dialog({ height: h, width: w, position: { my: "center bottom", at: "center bottom", of: containerDiv} }); //adds the dialog box
-	$("div.ui-dialog-titlebar").css("height", ".9em"); //reduces the height of the title bar
-	$("div.ui-dialog").css("font-size", "13px"); //reduces the text size of the message 
-	$("div.ui-dialog").css("border", "2px solid #DDD"); //adds a gray border
+	    var w = $(containerDiv).width() - 15;  //calculates the necessary width relative to the container div
+	    var h = $(containerDiv).height() * .30;  //calculates the necessary height relative to the container div
 
+	    $("#dialog").html(msgString.replace(/\n\r?/g, '<br />')); //replace all \n's with <br />'s, since \n is not recognized in the dialog box
+	    $("#dialog").dialog({ height: h, width: w, position: { my: "center bottom", at: "center bottom", of: containerDiv} }); //adds the dialog box
+		$("div.ui-dialog-titlebar").css("height", ".9em"); //reduces the height of the title bar
+		$("div.ui-dialog").css("font-size", "13px"); //reduces the text size of the message 
+		$("div.ui-dialog").css("border", "2px solid #DDD"); //adds a gray border
+	}
 
 }
+
+
 
 
