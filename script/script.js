3dee2ee8f6dd9ef8109a1061f6635aff73604dc4
dbetke <danielle.betke@gmail.com>
2012-10-18 14:41:34 -0400

    removes all jquery ui style and js, implements a basic div for a single message line, adds ability to receive two arguments and choose which to display to the user

--- script/script.js
+++ script/script.js
@@ -1,29 +1,53 @@
 
 function init(){
-	var msg = "This is a string of errors \n with a second line of errors \n and a third line of errors";  //assigning a message string for testing
-	
+	var msg = "This is a string of errors \n with a second line of errors \n and a third line of errors \n and a fourth \n and \n and \n and then \n and some more \n and even more \n and \n and \n and";  //assigning a message string for testing
+	var shortMsg = "This is a short message";
 	addErrors(msg);  //adds the error display message box on page load
 
 }
 
 
 
-function addErrors(msgString){
+function addErrors(msgString, shortMsgString){
+
+	var fullMsg =  msgString.replace(/\n\r?/g, '<br />'); //replace the unrecognized new line characters with line breaks 
+	var firstLine = function(){
+		var splitMsg = msgString.split("\n");
+		return splitMsg[0];
+		}
+		
+
+	var finalMsg = (shortMsgString != null) ? shortMsgString : firstLine; //if shortMsgString exists, use it for final message, else use msgString
+	var scrollboxClass = {	"font-family" :  "Helvetica, sans-serif", 
+							"background-color": "#CCCCFF",
+							"text-align" : "left", 
+							"font-size" : "10px",
+							"line-height" : "12px", 
+							"height" : "20px", 
+							"padding" : "5px", 
+							"overflow" : "auto",
+							"position": "relative"};
+
 
 	var containerDiv = "#" + $("#errorDisplay").parent().attr("id"); //finds the parent div to append the message dialog to
 
 	if (msgString != null){ //only create error message dialog if errors exist
 
 		$(containerDiv).append('<div id ="dialog" title="Errors"></div>'); //appends the errors div to the container declared above
+		$("#errorDisplay").addClass("scrollbox"); //add the class
+		$(".scrollbox").css(scrollboxClass); //add the css
+		$(".scrollbox").append(finalMsg); //append the message to the screen
 
-	    var w = $(containerDiv).width() - 15;  //calculates the necessary width relative to the container div
-	    var h = $(containerDiv).height() * .30;  //calculates the necessary height relative to the container div
+ 
+	    var h = $(containerDiv).height() - 40; //calculate to use for positioning relative to top of outer div
+	    var w = $(containerDiv).width() - 20; //calculate to use for setting the scrollbox width
+	    
 
-	    $("#dialog").html(msgString.replace(/\n\r?/g, '<br />')); //replace all \n's with <br />'s, since \n is not recognized in the dialog box
-	    $("#dialog").dialog({ height: h, width: w, position: { my: "center bottom", at: "center bottom", of: containerDiv} }); //adds the dialog box
-		$("div.ui-dialog-titlebar").css("height", ".9em"); //reduces the height of the title bar
-		$("div.ui-dialog").css("font-size", "13px"); //reduces the text size of the message 
-		$("div.ui-dialog").css("border", "2px solid #DDD"); //adds a gray border
+	    $(".scrollbox").css("top", h); //set the scrollbox to the bottom of the outer div utilizing the top attribute
+
+
+
+
 	}
 
 }
