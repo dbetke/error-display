e79299cb2060ddd0d3ddea40b991563cb4bf5d60
dbetke <danielle.betke@gmail.com>
2012-10-18 16:28:03 -0400

    adds additional div for x button and show details button, adds binded functions to the buttons, reorganizes code

--- script/script.js
+++ script/script.js
@@ -1,52 +1,76 @@
 
 function init(){
-	var msg = "This is a string of errors \n with a second line of errors \n and a third line of errors \n and a fourth \n and \n and \n and then \n and some more \n and even more \n and \n and \n and";  //assigning a message string for testing
+	//assigning temporary message strings for testing
+	var msg = "This is a string of errors \n with a second line of errors \n and a third line of errors \n and a fourth \n and \n and \n and then \n and some more \n and even more \n and \n and \n and \n blah blah blah";  
 	var shortMsg = "This is a short message";
-	addErrors(msg);  //adds the error display message box on page load
+	
+	//adds the error display message box on page load
+	addErrors(msg);  
 
 }
 
 
-
 function addErrors(msgString, shortMsgString){
-
-	var fullMsg =  msgString.replace(/\n\r?/g, '<br />'); //replace the unrecognized new line characters with line breaks 
+	//replace the unrecognized new line characters with line breaks 
+	var fullMsg =  msgString.replace(/\n\r?/g, '<br />'); 
+	
+	//split the full message and return the first line only to display
 	var firstLine = function(){
 		var splitMsg = msgString.split("\n");
 		return splitMsg[0];
 		}
+
+	//if shortMsgString exists, use it for final message, else use msgString
+	var finalMsg = (shortMsgString != null) ? shortMsgString : firstLine; 
+
+	//find the parent div to append the message dialog to
+	var containerDiv = "#" + $("#errorDisplay").parent().attr("id"); 
+
+	//only create error message dialog if errors exist
+	if (msgString != null){ 
+		$("head").append("<style> .xbutton { text-decoration : none; font-size : 15px} .detailsButton{margin-left : 10px}</style>"); //add css to the html page
 		
+		//add the scrollbox class with css to the error display div
+		$("#errorDisplay").addClass("scrollbox").css({"font-family" :  "Helvetica, sans-serif", 
+													  "background-color": "#CCCCFF",
+													  "text-align" : "left", 
+													  "font-size" : "12px",
+													  "line-height" : "12px", 
+													  "height" : "20px", 
+													  "padding" : "5px", 
+													  "position": "relative"}); 
+		//calculate to use for positioning relative to top of outer div
+		var h = $(containerDiv).height() - 40; 
+	  
+		//set the scrollbox to the bottom of the outer div utilizing the top attribute
+	    $(".scrollbox").css("top", h); 
 
-	var finalMsg = (shortMsgString != null) ? shortMsgString : firstLine; //if shortMsgString exists, use it for final message, else use msgString
-	var scrollboxClass = {	"font-family" :  "Helvetica, sans-serif", 
-							"background-color": "#CCCCFF",
-							"text-align" : "left", 
-							"font-size" : "10px",
-							"line-height" : "12px", 
-							"height" : "20px", 
-							"padding" : "5px", 
-							"overflow" : "auto",
-							"position": "relative"};
+	    //add the options div to the error display div 
+		$("#errorDisplay").append("<div id ='options'></div>"); 
 
+		//assign css to the options div
+		$("#options").css({ "text-align" : "right",
+							"float" : "right"}); 
 
-	var containerDiv = "#" + $("#errorDisplay").parent().attr("id"); //finds the parent div to append the message dialog to
+		//append the show details button
+		$("#options").append("<button id='details' class='detailsButton'>See Details</button>&nbsp;&nbsp;"); 
 
-	if (msgString != null){ //only create error message dialog if errors exist
+		//append the x button
+		$("#options").append("<a href='' class='xbutton'>&#10006;</a>"); 
 
-		$(containerDiv).append('<div id ="dialog" title="Errors"></div>'); //appends the errors div to the container declared above
-		$("#errorDisplay").addClass("scrollbox"); //add the class
-		$(".scrollbox").css(scrollboxClass); //add the css
-		$(".scrollbox").append(finalMsg); //append the message to the screen
+		//add the single-line message to the screen
+		$(".scrollbox").append(finalMsg); 
+		
+		
+		//set the x button click event
+		$(".xbutton").bind('click', function(event) {
+    			alert("closed!");
+		});
 
- 
-	    var h = $(containerDiv).height() - 40; //calculate to use for positioning relative to top of outer div
-	    var w = $(containerDiv).width() - 20; //calculate to use for setting the scrollbox width
-	    
-
-	    $(".scrollbox").css("top", h); //set the scrollbox to the bottom of the outer div utilizing the top attribute
-
-
-
+		//set the show details button click event
+		$(".detailsButton").bind('click', function(event) {
+    			alert(msgString);
+		});
 
 	}
 
