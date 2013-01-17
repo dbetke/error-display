ac412acc5fc681963a0c4511f6e656ca0310b9e5
dbetke <danielle.betke@gmail.com>
2013-01-17 13:22:13 -0500

    updates build

--- build/errorDisplay.js
+++ build/errorDisplay.js
@@ -28,7 +28,38 @@
             + '<ul class="errorDisplayFullMessageList">'
             + '</ul>'
     );
+
     
+    /** templates **/
+    
+
+
+    var singleLineMessageTemplate = (
+        ' <div class="errorDisplayDetailsListItem">' +
+        '   <span class ="{{class}}">&bull; {{message}}</span>' + 
+	' </div>'
+    );
+
+    
+    var multiLineMessageOuterTemplate = (
+       ' <div class="errorDisplayDetailsListItem">' +
+       '   <div class="errorDisplayDetailsListItemCollapsed"></div>' +
+       '   <div class="errorDisplayDetailsListItemExpanded"></div>' +
+       ' </div>'
+    );
+    
+    var multiLineMessageAdditionalLineTemplate = (
+      ' <span class="{{class}}">{{bullet}} {{message}}</span>'
+    );
+   
+
+    var multiLineMessageFirstLineTemplate = (
+        ' <div class="errorDisplayDetailsListItem">' +
+        '   <span class ="{{class}}">{{bullet}} {{message}}</span>' + 
+	' </div>'
+    );
+
+
     var methods = {
         init : function(options) {
             return this.each(function() {
@@ -140,21 +171,23 @@
                 //process for multi-line messages
                 var splitMsg = fullMessage.split("\n"); 
                 var finalMsg;
-                 
-                if (splitMsg.length > 1){
-                     $(data.detailDisplayList).append($('<li class="errorDisplayExpandable">'+errorDisplayTimestamp+" " +shortMessage+'</li>').css('color', settings.displayFontColor));
-                     
-                      for (var i=0; i<splitMsg.length; i++){
-                        finalMsg = '<li class="errorDisplayExpandedChildren">' +  splitMsg[i] + '</li>';  
-                        $(data.detailDisplayList).append($(finalMsg).css('color', settings.displayFontColor));
-                      }
-                }
-                   
-                else{
-                     $(data.detailDisplayList).append($('<li>'+errorDisplayTimestamp+" " +fullMessage+'</li>').css('color', settings.displayFontColor));
-                }
-        
-      
+                
+                var obj;
+                
+		if (splitMsg.length == 1){
+		    obj = $(Mustache.to_html(singleLineMessageTemplate, {'class' : 'plain', 'bullet' : 'o', 'message' : errorDisplayTimestamp + ' ' + splitMsg[0] })).css('color', settings.displayFontColor).appendTo($(data.detailDisplayList));
+		}
+
+		else{
+		    obj = $(multiLineMessageOuterTemplate).appendTo($(data.detailDisplayList));
+		    obj.find('.errorDisplayDetailsListItemCollapsed').append(Mustache.to_html(multiLineMessageFirstLineTemplate, {'class' : 'collapsed', 'bullet' : '+', 'message' : errorDisplayTimestamp + ' ' + splitMsg[0] })).css('color', settings.displayFontColor);
+		    obj.find('.errorDisplayDetailsListItemExpanded').append(Mustache.to_html(multiLineMessageFirstLineTemplate, {'class' : 'expanded', 'bullet' : '-', 'message' : errorDisplayTimestamp + ' ' + splitMsg[0] })).css('color', settings.displayFontColor);		    
+		    for (var i=1; i<splitMsg.length; i++){
+			obj.find('.errorDisplayDetailsListItemExpanded').append(Mustache.to_html(multiLineMessageAdditionalLineTemplate, {'class' : 'additional', 'bullet' : ' ', 'message' : splitMsg[i] })).css('color', settings.displayFontColor);
+		    }
+		    setupClickHandlers(obj);
+		}
+
                 if(settings.displayLocation == 'top'){
                   $this.find('.errorDisplay').removeClass('errorDisplayBottom').addClass('errorDisplayTop');
                   $this.find('.errorDisplayRetriever').removeClass('errorDisplayRetrieverBottom').addClass('errorDisplayRetrieverTop');
@@ -165,8 +198,8 @@
                   $this.find('.errorDisplayRetriever').removeClass('errorDisplayRetrieverTop').addClass('errorDisplayRetrieverBottom');      
                 }
                 
-                $this.find('.errorDisplayRetriever').hide().css('background-color', settings.displayIndicatorColor);
-                $this.find('.errorDisplayOptions').hide().css('background-color', settings.displayBackgroundColor);
+                $this.find('.errorDisplayRetriever').hide();
+                $this.find('.errorDisplayOptions').hide();
                 $this.find('.errorDisplayShortMessage').css('color', settings.displayFontColor).html(errorDisplayShortMessage); 
                 
                 if( $('.errorDisplayDetailsOuter').css("display") == 'none' ){  //only show error display div if details are not open
@@ -193,6 +226,18 @@
                       $this.find('.errorDisplay').show();
                     }
                 } 
+		
+		function setupClickHandlers(obj){
+		    obj.find('span.expanded').click(function(){
+			    obj.find('.errorDisplayDetailsListItemExpanded').hide();
+			    obj.find('.errorDisplayDetailsListItemCollapsed').show();
+			});
+		    obj.find('span.collapsed').click(function(){
+			    obj.find('.errorDisplayDetailsListItemExpanded').show();
+			    obj.find('.errorDisplayDetailsListItemCollapsed').hide();
+			});
+			
+		}
 
                // $(data.detailDisplayList).append($('<li>'+errorDisplayTimestamp+" " +fullMessage+'</li>').css('color', settings.displayFontColor));
 
@@ -213,4 +258,4 @@
     };
 
 }(jQuery));
-jQuery('head').append(jQuery('<style type="text/css">.errorDisplay { font-family: Helvetica, sans-serif; color: #32446B; background-color: white; text-align: left; font-size: 12px; line-height: 12px; height: 45px; left: 0px; width: inherit; width: expression(this.parentNode.currentStyle[\'width\']); border: 3px solid #4D68A3; display: none; border-radius: 10px; } .errorDisplayTop{ position: relative; } .errorDisplayBottom{ position: absolute; bottom: 0px; } .errorDisplayRetriever{ height: 8px; background-color: #ff0000; width: 8px; position: absolute; left: 0px; display: none; } .errorDisplayRetrieverTop{ border-bottom-right-radius: 10px; } .errorDisplayRetrieverBottom{ bottom: 0px; border-top-right-radius: 10px;; } .errorDisplayShortMessage{ overflow-x: hidden; overflow-y: hidden; position: absolute; bottom: 0px; left: 0px; width: 90%; margin-top: 20px; margin-left: 10px; margin-bottom: 5px; display: inline; height: 13px; } .errorDisplayOverflow{ position: absolute; right: 7px; width: 5%; bottom: 0px; margin-bottom: 5px; display: none; } .errorDisplayXButton { text-decoration: none; font-size: 15px; margin-top: 2px; position: absolute; right: 2px; top: 0px; color: #4D68A3; } .errorDisplayDetailsButton { margin-left: 10px; margin-right: 10px; position: relative; } .errorDisplayDetailsOuter{ position: absolute; top: 25%; left: 25%; width: 630px; height: 100px; display: none; } .errorDisplayDetails{ width: 600px; height: 100px; border: 2px solid #4D68A3; border-radius: 10px; background-color: white; } .errorDisplayDetailsXButton { border: 2px solid #c2c2c2; position: absolute; right: 0px; top: 0px; padding: 1px 5px; background-color: #4D68A3; border-radius: 20px; text-decoration: none; font-size: 13px; color: white; } .errorDisplayFullMessageArea { font-family: Helvetica, sans-serif; font-size: .833em; color: #32446B; height: 80px; width: inherit; width: expression(this.parentNode.currentStyle[\'width\']); margin-top: 15px; } .errorDisplayFullMessageList { overflow: auto; white-space: nowrap; height: 80px; margin-top: 5px; } .errorDisplayOptions{ background-color: #FFFFFF; display: inline; } </style>'));
+jQuery('head').append(jQuery('<style type="text/css">.errorDisplay { font-family: Helvetica, sans-serif; color: #32446B; background-color: white; text-align: left; font-size: 12px; line-height: 12px; height: 45px; left: 0px; width: inherit; width: expression(this.parentNode.currentStyle[\'width\']); border: 3px solid #4D68A3; display: none; border-radius: 10px; } .errorDisplayTop{ position: relative; } .errorDisplayBottom{ position: absolute; bottom: 0px; } .errorDisplayRetriever{ height: 8px; background-color: #ff0000; width: 8px; position: absolute; left: 0px; display: none; } .errorDisplayRetrieverTop{ border-bottom-right-radius: 10px; } .errorDisplayRetrieverBottom{ bottom: 0px; border-top-right-radius: 10px;; } .errorDisplayShortMessage{ overflow-x: hidden; overflow-y: hidden; position: absolute; bottom: 0px; left: 0px; width: 90%; margin-top: 20px; margin-left: 10px; margin-bottom: 5px; display: inline; height: 13px; } .errorDisplayOverflow{ position: absolute; right: 7px; width: 5%; bottom: 0px; margin-bottom: 5px; display: none; } .errorDisplayXButton { text-decoration: none; font-size: 15px; margin-top: 2px; position: absolute; right: 2px; top: 0px; color: #4D68A3; } .errorDisplayDetailsButton { margin-left: 10px; margin-right: 10px; position: relative; } .errorDisplayDetailsOuter{ position: absolute; top: 25%; left: 25%; width: 630px; height: 100px; display: none; } .errorDisplayDetails{ width: 600px; height: 100px; border: 2px solid #4D68A3; border-radius: 10px; background-color: white; } .errorDisplayDetailsXButton { border: 2px solid #c2c2c2; position: absolute; right: 0px; top: 0px; padding: 1px 5px; background-color: #4D68A3; border-radius: 20px; text-decoration: none; font-size: 13px; color: white; } .errorDisplayFullMessageArea { font-family: Helvetica, sans-serif; font-size: .833em; color: #32446B; height: 80px; width: inherit; width: expression(this.parentNode.currentStyle[\'width\']); margin-top: 15px; } .errorDisplayFullMessageList { overflow: auto; white-space: nowrap; height: 80px; margin-top: 5px; } .errorDisplayOptions{ background-color: #FFFFFF; display: inline; } span.errorDisplayDetailsPlain{ margin-left: 2px; } errorDisplayDetailsListItemCollapsed, errorDisplayDetailsListItemExpanded{ display: inline-block; } span.additional{ margin-left: 10px; } .errorDisplayDetailsListItemExpanded{ display: none; }</style>'));
